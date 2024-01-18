bb94da4890d105f275fb820846b90cf2b33c5a6d
arya <aryatheruparambil@gmail.com>
2024-01-18 09:08:41 +0530

    file added

--- Todoproject/userapp/views.py
+++ Todoproject/userapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.app.models import user
+from django.contrib import app
 
 # Create your views here.
 def registerUser(req):
@@ -9,5 +12,20 @@ def registerUser(req):
         username=req.POST.get("username","")
         password=req.POST.get("password","")
         cpassword=req.POST.get("cpassword","")
-        print(fname,lname,email,username,password,cpassword)
+        if password==cpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(req,"username already exist")
+                return redirect('app:register')
+            if User.objects.filter(email=email).exists():
+                messages.info(req,"email already exist")
+                return redirect('app:register')
+            else:
+                user=user.objects.create_user(first_name=fname,last_name=lname,email=email,username=username,password=password)
+                user.save()
+                return redirect('home')
+        else:
+            messages.info(req,"password not matched")
+            return redirect('app:register')    
+            
+                
     return render(req,'registeruser.html')        
